15c0176415559337a1c014249a3ff8c129d075b1
beniamino green <beniamino.green@tuta.com>
2025-12-05 09:10:48 -0500

    added stan work to make predictions

--- derail_model.stan
+++ derail_model.stan
@@ -1,8 +1,11 @@
 data {
   int <lower=0> N_models;
+  int <lower=0> N_test;
   int <lower=0> N;
   int model_a[N];
   int model_b[N];
+  int validation_model_a[N_test];
+  int validation_model_b[N_test];
   int winner[N]; // 1 (loss), 2 (tie), or 3 (win)
   int both_bad[N];
   vector [N] num;
@@ -34,7 +37,6 @@ model {
 generated quantities {
  matrix [N, 4]  preds;
  for (i in 1:N) {
-
   // Useful Quantities
   real p_either_fail = failure_probability[model_a[i]] + failure_probability[model_b[i]] - failure_probability[model_a[i]]*failure_probability[model_b[i]];
   real p_neither_fail = 1 - p_either_fail;
@@ -48,4 +50,21 @@ generated quantities {
   // probability of both bad
   preds[i,4] = p_either_fail;
   }
+
+ matrix [N_test, 4] validation; 
+ for (i in 1:N_test) {
+    // Useful Quantities
+    real p_either_fail = failure_probability[validation_model_a[i]] + failure_probability[validation_model_b[i]] - failure_probability[validation_model_a[i]]*failure_probability[validation_model_b[i]];
+    real p_neither_fail = 1 - p_either_fail;
+
+    // probability of A winning - either b only fails or neither fails and a wins
+    preds[i,1] = p_neither_fail * (1 - inv_logit(theta[validation_model_a[i]] -theta[validation_model_b[i]] - cutpoints[1]));
+    // probability of B winning
+    preds[i,2] = p_neither_fail * (inv_logit(theta[validation_model_a[i]] -theta[validation_model_b[i]] - cutpoints[2]));
+    // probability of tie
+    preds[i,3] = p_neither_fail * (inv_logit(theta[validation_model_a[i]] -theta[validation_model_b[i]] - cutpoints[1]) - inv_logit(theta[validation_model_a[i]] -theta[validation_model_b[i]] - cutpoints[2]));
+    // probability of both bad
+    preds[i,4] = p_either_fail;
+  }
+
 }
--- stan_modelling_2.R
+++ stan_modelling_2.R
@@ -1,35 +1,64 @@
 library(arrow)
 library(tidyverse)
 library(rstan)
+library(patchwork)
 
-parquet_files <- list.files(pattern = "*.parquet")
+####### Jay's Code from here ...
+# Multiple file read & combine...
 
-df <- open_dataset(parquet_files) %>%
+files <- dir(pattern = "parquet")
+z <- list()
+for (i in 1:length(files)) {
+  # modification I made: this is faster than read_paruqet becuase 
+  # it does not require pulling every column into memory
+  x <- open_dataset(files[i]) %>% 
+    select(id, model_a, model_b, winner) %>% 
+    collect()
+  z[[i]] <- x
+}
+x <- do.call(rbind, z)
+saveRDS(x, file = 'chatbot.data.rds')
+
+rm(list = ls())
+x <- readRDS(file = 'chatbot.data.rds')
+
+# dim(x)
+# head(x)
+# table(x$winner)
+# identical(sort(unique(x$model_a)), sort(unique(x$model_b)))  # Nice.
+
+# Use this to gather 100 target matchups for prediction (see below).
+# Minor modifications may be needed but if you use my set.seed(1)
+# then every group should be making predictions for the same set
+# of 100 matchups.
+
+set.seed(1)
+numextras <- 100
+
+prediction_set <-x[sample(nrow(x), numextras),]
+prediction_set$winner <- NA
+
+df <- x
+
+############################ to here
+
+df <- df %>% 
   group_by(model_a, model_b, winner) %>%
   summarize(n = n()) %>%
   collect() %>%
   drop_na()
 
-df_no_ties <- df %>%
-  filter(!winner %in% c("tie", "both_bad"))
 model_names <- unique(c(df$model_a, df$model_b))
 n_models <- length(model_names)
 model_lookup <- 1:n_models
 names(model_lookup) <- model_names
-
-stan_df_no_ties <- df_no_ties %>%
-  mutate(
-    model_a = model_lookup[model_a],
-    model_b = model_lookup[model_b],
-    winner = as.numeric(winner == "model_a")
-  )
 
 stan_df <- df %>%
   mutate(
     model_a = model_lookup[model_a],
     model_b = model_lookup[model_b],
     winner = case_match(
-      winner,
+      as.character(winner),
       "model_a" ~ 3,
       "tie" ~ 2,
       "model_b" ~ 1,
@@ -45,13 +74,38 @@ stan_data <- list(
   model_b = stan_df$model_b,
   winner = stan_df$winner,
   both_bad = stan_df$both_bad,
-  num = stan_df$n
+  num = stan_df$n,
+  N_test = nrow(prediction_set), 
+  validation_model_a = model_lookup[prediction_set$model_a],
+  validation_model_b = model_lookup[prediction_set$model_b]
 )
 
-both_model <-  stan_model("derail_model.stan")
 
+create_leaderboard_plot <- function(model_file) {
+  model_object <- stan_model(model_file)
 
-create_matchup_df <- function(model_file) {
+  MAP <- optimizing(
+    model_object, 
+    data = stan_data
+  )
+
+  thetas <- MAP$par[grepl("theta", names(MAP$par))]
+  names(thetas) <- names(model_lookup)
+
+  tibble(
+    model = names(thetas),
+    score = thetas
+  )  %>% 
+    ggplot(aes(x= reorder(model, score), y = score)) + 
+    coord_flip() + 
+    geom_point() + 
+    ggtitle("Model Leaderboard") + 
+    xlab("Model Name") + 
+    ylab("Model Score")  + 
+    theme_bw()
+}
+
+create_matchup_df <- function(model_file, stan_data) {
   model_object <- stan_model(model_file)
 
   MAP <- optimizing(
@@ -101,86 +155,100 @@ ce_loss <- function(df) {
 }
 
 create_plots <- function(df, model_name = NULL) {
-  a_win_plot <- ggplot(df) + 
-    geom_point(
-      aes(x=pred_a_win, y=perc_a_win), 
-    ) + 
+  a_win_plot <- ggplot(df, aes(x=pred_a_win, y=perc_a_win)) + 
+    geom_point(alpha = .15) + 
     geom_abline(slope=1, linetype = 2) + 
-    ggtitle(
-      "Precicted VS Actual Probabilities ",
-      subtitle = model_name
-    ) + 
+    geom_smooth(method = "lm", se = F) +
+    ggtitle("Model A Wins") + 
     xlab("Predicted Probability of Model A winning") + 
     ylab("Observed Probability of Model A winning") + 
     scale_x_continuous(limits = c(0,1)) + 
     scale_y_continuous(limits = c(0,1)) + 
     theme_bw()
 
-  b_win_plot <- ggplot(df) + 
-    geom_point(
-      aes(x=pred_b_win, y=perc_b_win), 
-    ) + 
+  b_win_plot <- ggplot(df, aes(x=pred_b_win, y=perc_b_win)) + 
+    geom_point(alpha = .15) +  
     geom_abline(slope=1, linetype = 2) + 
-    ggtitle(
-      "Precicted VS Actual Probabilities ",
-      subtitle = model_name
-    ) + 
+    geom_smooth(method = "lm", se = F ) +
+    ggtitle("Model B Wins") + 
     xlab("Predicted Probability of Model B winning") + 
     ylab("Observed Probability of Model B winning") + 
     scale_x_continuous(limits = c(0,1)) + 
     scale_y_continuous(limits = c(0,1)) + 
     theme_bw()
 
-  tie_plot <- ggplot(df) + 
-    geom_point(
-      aes(x=pred_tie, y=perc_tie), 
-    ) + 
+  tie_plot <- ggplot(df,aes(x=pred_tie, y=perc_tie)) + 
+    geom_point(alpha = .15) + 
+    geom_smooth(method = "lm", se = F ) +
     geom_abline(slope=1, linetype = 2) + 
-    ggtitle(
-      "Precicted VS Actual Probabilities ",
-      subtitle = model_name
-    )  + 
+    ggtitle("Tie")  + 
     xlab("Predicted Probability of tie") + 
     ylab("Observed Probability of tie") + 
     scale_x_continuous(limits = c(0,1)) + 
     scale_y_continuous(limits = c(0,1)) + 
     theme_bw()
 
-  bb_plot <- ggplot(df) + 
-    geom_point(
-      aes(x=pred_bb, y=perc_bb), 
-    ) + 
+  bb_plot <- ggplot(df, aes(x=pred_bb, y=perc_bb)) + 
+    geom_point(alpha = .15) + 
     geom_abline(slope=1, linetype = 2) + 
-    ggtitle(
-      "Precicted VS Actual Probabilities ",
-      subtitle = model_name
-    ) +
+    geom_smooth(method = "lm", se = F ) +
+    ggtitle("Both Bad") +
     xlab("Predicted Probability of both bad outcome") + 
     ylab("Observed Probability of both bad outcome") + 
     scale_x_continuous(limits = c(0,1)) + 
     scale_y_continuous(limits = c(0,1)) + 
     theme_bw()
 
-  list(
+  plots <- list(
     a_win_plot, 
     b_win_plot,
     tie_plot, 
     bb_plot
   )
+
+  ce_loss <- round(ce_loss(df),2)
+
+  wrap_plots(plots) +
+    plot_annotation(
+      title = str_glue("Calibration Plot for {model_name} Model"), 
+      subtitle = str_glue("Cross-Entropy Loss: {ce_loss}")
+    )
 }
 
-matchups_d <- create_matchup_df("derail_model.stan")
-ce_loss(matchups_d)
-plts <- create_plots(matchups_d, "Model 2")
+counts <- stan_df %>% 
+  uncount(n) %>% 
+  group_by(winner) %>% 
+  summarize(n=n()) %>% 
+  pull(n)
+naive_probs <- counts/sum(counts)
 
-matchups_b <- create_matchup_df("model_both.stan")
-ce_loss(matchups_b)
+constant_forecast <- stan_df %>% 
+  uncount(n) %>% 
+  group_by(model_a, model_b) %>% 
+  select(-both_bad) %>% 
+  summarize(
+    perc_a_win = sum(winner == 1) / n(),
+    perc_b_win = sum(winner == 3) / n(),
+    perc_tie = sum(winner == 2) / n(),
+    perc_bb = sum(winner == 99) / n(),
+    pred_a_win = naive_probs[1],
+    pred_b_win = naive_probs[2],
+    pred_tie = naive_probs[3],
+    pred_bb = naive_probs[4]
+  )
+
+plts <- create_plots(constant_forecast, "Constant Forecast")
+ggsave("calibration_plots/baseline.png", plts, width = 8, height = 8)
+matchups_d <- create_matchup_df("derail_model.stan", stan_data)
+plts <- create_plots(matchups_d, "Derail Model")
+ggsave("calibration_plots/derail_model_calibration.png", plts, width = 8, height = 8)
+matchups_b <- create_matchup_df("model_both.stan", stan_data)
 plts <- create_plots(matchups_b, "model 1")
+ggsave("calibration_plots/both_model_calibration.png", plts,  width = 8, height = 8)
+matchups_c <- create_matchup_df("common_faliure_prob.stan", stan_data)
+plt <- create_plots(matchups_c, "Global Both Bad Probability")
+ggsave("calibration_plots/common_faliure_prob_calibration.png", plts, width = 8, height = 8)
 
-matchups_c <- create_matchup_df("common_faliure_prob.stan")
-ce_loss(matchups_c)
-plts <- create_plots(matchups_c, "model 1")
-plts[[3]]
 
 
 
